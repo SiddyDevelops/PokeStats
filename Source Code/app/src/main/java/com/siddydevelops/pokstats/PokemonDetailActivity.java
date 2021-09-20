8c30214c9424935780c40505490dbddb8d7ecdac
Siddharth Singh <siddhartsingh2002@gmail.com>
2021-09-20 07:56:46 +0530

    Siddy's -v 1.2.5

--- Source Code/app/src/main/java/com/siddydevelops/pokstats/PokemonDetailActivity.java
+++ Source Code/app/src/main/java/com/siddydevelops/pokstats/PokemonDetailActivity.java
@@ -3,9 +3,15 @@ package com.siddydevelops.pokstats;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.siddydevelops.pokstats.Models.PokeDetail;
 import com.siddydevelops.pokstats.Models.PokeStats;
 import com.siddydevelops.pokstats.PokeApi.PokeApiService;
@@ -24,13 +30,31 @@ public class PokemonDetailActivity extends AppCompatActivity {
 
     //https://pokeapi.co/api/v2/pokemon/{poke_num}  --> DETAIL API
 
+    TextView nameOfPokemon;
+    ImageView pokemonImageView;
+    TextView heightTextView;
+    TextView weightTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pokemon_detail);
 
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        //int colorCodeDark = Color.parseColor("#FF9800");
+        window.setStatusBarColor(getResources().getColor(R.color.pokemonColor1));
+
         Intent intent = getIntent();
         int pokeNum = intent.getIntExtra("pokeNum",1);
+
+        nameOfPokemon = findViewById(R.id.nameOfPokemon);
+        pokemonImageView = findViewById(R.id.pokemonImageView);
+        heightTextView = findViewById(R.id.heightTextView);
+        weightTextView = findViewById(R.id.weightTextView);
+
+        Glide.with(this).load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"+ pokeNum +".png").into(pokemonImageView);
+
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://pokeapi.co/api/v2/")
@@ -54,6 +78,10 @@ public class PokemonDetailActivity extends AppCompatActivity {
                 Log.i("Weight", pokeDetails.getWeight());
                 List<PokeStats> p = pokeDetails.getStats();
                 Log.i("ListDATA", p.toString());
+
+                nameOfPokemon.setText(pokeDetails.getName());
+                heightTextView.setText(pokeDetails.getHeight() + " KG");
+                weightTextView.setText(pokeDetails.getWeight() + " m");
 
             }
 
